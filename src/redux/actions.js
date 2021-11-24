28723b43047539f391e86dc734f4ff043dbcc5f6
Zmennn <79196210+Zmennn@users.noreply.github.com>
2021-11-24 18:14:48 +0200

    0.3

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,3 @@
 import { createAction } from '@reduxjs/toolkit';
 
-export const deleteRecord = createAction('deleteRecord');
 export const changeFilter = createAction('changeFilter');
-export const submitRecord = createAction('submitRecord');
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,28 +1,9 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './reducers';
-import logger from 'redux-logger';
-
-const middleware = [...getDefaultMiddleware(), logger];
-
-const initData = () => {
-  const data = localStorage.getItem('contacts');
-  if (data) {
-    return JSON.parse(data);
-  } else {
-    return [];
-  }
-};
-
-const preloadState = {
-  contacts: initData(),
-  filter: '',
-};
 
 const store = configureStore({
   reducer: reducer,
-  middleware: middleware,
   devTools: process.env.NODE_ENV === 'development',
-  preloadedState: preloadState,
 });
 
 export default store;
